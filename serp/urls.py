4ecb0b24cb95f9e66a7ea1d224ef74555f8da157
sirdesmond09 <sirdesmond09@gmail.com>
2021-07-09 11:46:18 +0100

    url route

--- serp/urls.py
+++ serp/urls.py
@@ -22,7 +22,7 @@ schema_view = get_schema_view( # new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/', include('main.urls', 'main')),
-    path('docs/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('docs/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
